90195f6ecd8a241d3d65be871869bbc98fa5a4de
zaabsyid <abdulrosyid2002@gmail.com>
2022-09-14 16:17:37 +0700

    initial commit with navigation component paling new

--- app/src/main/java/com/zahirar/tugasch4tpc1viewmodel/DetailFragment.kt
+++ app/src/main/java/com/zahirar/tugasch4tpc1viewmodel/DetailFragment.kt
@@ -1,13 +1,12 @@
 package com.zahirar.tugasch4tpc1viewmodel
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
-import kotlinx.android.synthetic.main.activity_detail_berita.*
+import kotlinx.android.synthetic.main.fragment_detail.*
 
 class DetailFragment : Fragment() {
 
